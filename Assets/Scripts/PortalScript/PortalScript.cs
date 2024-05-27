c8e7b29bfe9683585b5301de82c16ab5512e17a0
nello27 <yesid.olaya@gmail.com>
2024-05-27 17:56:37 -0500

    cargando portales

--- Assets/Scripts/PortalScript/PortalScript.cs
+++ Assets/Scripts/PortalScript/PortalScript.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class PortalScript : MonoBehaviour
 {
+    private GameObject player;
     public GameObject portalBlue;
+    public GameObject portalBlack;
+    public GameObject portalYellow;
+    public Rigidbody2D rb;
+    public float pushForce = 10f; // Fuerza de empuje en el eje X
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag(MyTags.PLAYER_TAG);
+        rb = player.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -16,4 +22,42 @@ public class PortalScript : MonoBehaviour
     {
         
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Verifica que el objeto que colisiona tiene el tag del jugador
+        if (collision.gameObject.tag == MyTags.PLAYER_TAG)
+        {
+
+            // Obtén el nombre del objeto que tiene este script
+            string thisObjectName = gameObject.name;
+
+            if (thisObjectName == "portalBlue") {
+
+                // Teletransporta el objeto a la posición del portal negro
+                collision.gameObject.transform.position = portalBlack.transform.position;
+
+
+                rb.AddForce(Vector2.up * pushForce, ForceMode2D.Impulse); // Aplica fuerza hacia arriba
+
+            }
+
+            if (thisObjectName == "portalBlack")
+            {
+
+                // Teletransporta el objeto a la posición del portal negro
+                collision.gameObject.transform.position = portalYellow.transform.position;
+
+
+                rb.AddForce(Vector2.up * pushForce, ForceMode2D.Impulse); // Aplica fuerza hacia arriba
+
+            }
+
+
+
+        }
+    }
+
+
+
 }
