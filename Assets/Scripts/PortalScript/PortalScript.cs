1ae18819283be195ff16a4f235fe0ca641d8f113
nello27 <yesid.olaya@gmail.com>
2024-05-28 11:18:30 -0500

    Acomodando Script Portal

--- Assets/Scripts/PortalScript/PortalScript.cs
+++ Assets/Scripts/PortalScript/PortalScript.cs
@@ -8,13 +8,16 @@ public class PortalScript : MonoBehaviour
     public GameObject portalBlue;
     public GameObject portalBlack;
     public GameObject portalYellow;
+    public GameObject portalRed;
     public Rigidbody2D rb;
-    public float pushForce = 10f; // Fuerza de empuje en el eje X
+    private float pushForce = 20f; // Fuerza de empuje en el eje X
+    public int countPortal;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag(MyTags.PLAYER_TAG);
         rb = player.GetComponent<Rigidbody2D>();
+        countPortal = 0;
     }
 
     // Update is called once per frame
@@ -38,26 +41,58 @@ public class PortalScript : MonoBehaviour
                 collision.gameObject.transform.position = portalBlack.transform.position;
 
 
-                rb.AddForce(Vector2.up * pushForce, ForceMode2D.Impulse); // Aplica fuerza hacia arriba
+                //rb.AddForce(Vector2.up * pushForce, ForceMode2D.Impulse); // Aplica fuerza hacia arriba
 
             }
 
             if (thisObjectName == "portalBlack")
             {
+                
 
-                // Teletransporta el objeto a la posición del portal negro
-                collision.gameObject.transform.position = portalYellow.transform.position;
+                if (countPortal >= 1)
+                {
+
+                    // Teletransporta el objeto a la posición del portal negro
+                    collision.gameObject.transform.position = portalYellow.transform.position;
 
 
-                rb.AddForce(Vector2.up * pushForce, ForceMode2D.Impulse); // Aplica fuerza hacia arriba
-
+                    //rb.AddForce(Vector2.up * pushForce, ForceMode2D.Impulse); // Aplica fuerza hacia arriba
+                    countPortal = 0;
+                }
+                else
+                {
+                    countPortal++;
+                }
+                
             }
 
+            if (thisObjectName == "portalYellow")
+            {
 
+
+                if (countPortal >= 1)
+                {
+
+                    // Teletransporta el objeto a la posición del portal negro
+                    collision.gameObject.transform.position = portalRed.transform.position;
+
+
+                    rb.AddForce(Vector2.up * pushForce, ForceMode2D.Impulse); // Aplica fuerza hacia arriba
+                    countPortal = 0;
+                }
+                else
+                {
+                    countPortal++;
+                }
+
+            }
 
         }
     }
 
+    private void OnTriggerExit2D(Collider2D collision)
+    {
 
+    }
 
 }
